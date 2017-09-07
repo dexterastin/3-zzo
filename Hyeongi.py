f6291d6f47f77f285e3b236543d81593f94e6fe4
sonhyeongi <forever2331@kookmin.ac.kr>
2017-09-07 13:06:52 +0900

    second

--- Hyeongi.py
+++ Hyeongi.py
@@ -8,3 +8,19 @@ while (True):
 		for i in range (1,n+1):
 			result = i * result
 		print(result)
+---------------------------------------------------
+while (True):
+        result = 1
+        try:
+            n = int(input("Input your number : "))
+        except ValueError:
+            print("문자열 또는 자연수가 아닌 수가 입력되었습니다. 자연수를 입력해주세요.")
+        else:
+            if(n==-1):
+                    break
+            elif (n<-1):
+                print("음수가 입력되었습니다. 자연수를 입력해주세요.")
+            else:
+                    for i in range (1,n+1):
+                            result = i * result
+                    print(result)
