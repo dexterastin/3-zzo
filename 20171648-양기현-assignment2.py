56deb2a34e0b0b83c777fd563b24cbd9303773b5
Gihyeon Yang <dexterastin@gmail.com>
2017-09-07 11:41:19 +0900

    Update 20171648-양기현-assignment2.py

--- 20171648-양기현-assignment2.py
+++ 20171648-양기현-assignment2.py
@@ -1,9 +1,11 @@
 while 1:
-    n = int(input("Enter a number: "))
+    n = input("Enter a number: ")
 
-    if not str(n).isdigit():
+    if not n.isdigit() and n != "-1":
         print("Please enter value correctly.")
         continue
+
+    n = int(n)
 
     if n == -1:
         break
