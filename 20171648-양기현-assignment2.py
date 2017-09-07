61cb97416d7e97c533a8f77aa5e45a80110b4aa9
Gihyeon Yang <dexterastin@gmail.com>
2017-09-07 11:42:38 +0900

    Update 20171648-양기현-assignment2.py

--- 20171648-양기현-assignment2.py
+++ 20171648-양기현-assignment2.py
@@ -4,15 +4,18 @@ while 1:
     if not n.isdigit() and n != "-1":
         print("Please enter value correctly.")
         continue
+        pass
 
     n = int(n)
 
     if n == -1:
         break
+        pass
 
     factorial = 1
 
     for value in range(1, n + 1):
         factorial *= value
+        pass
 
     print("%d! = %d" % (n, factorial))
