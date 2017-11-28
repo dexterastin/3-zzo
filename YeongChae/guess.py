ce073081ee34a2074f05ebd2abaaa5b9ba131020
Seo-yeong-Chae <tjdudco10@daum.net>
2017-11-28 10:39:41 +0900

    Let's make hangman game~!

---
+++ YeongChae/guess.py
@@ -0,0 +1,40 @@
+class Guess:
+
+    def __init__(self, word):
+        self.secretWord=word
+        self.currentStatus=[]
+        for idx in range(len(self.secretWord)):
+            self.currentStatus.append("_")
+        self.guessedChars = set()
+        self.numTries=0
+
+
+    def display(self):
+        print("Current: "+ (''.join(self.currentStatus)))
+        print("Tries: ", self.numTries)
+
+
+    def guess(self, character):
+        self.guessedChars.add(character)
+        self.fIdx=0
+        i=0
+
+        while i < len(self.secretWord):
+            if character==self.secretWord[i]:
+                self.currentStatus[i] = self.secretWord[i]
+                i += 1
+                self.fIdx = self.fIdx + 1
+            else:
+                i=i+1
+
+        if self.fIdx==0:
+            self.numTries=self.numTries+1
+
+        if ''.join(self.currentStatus)==self.secretWord:
+            return True
+
+        else:
+            return False
+
+
+
